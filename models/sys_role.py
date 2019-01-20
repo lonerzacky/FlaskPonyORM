629d1eda2b0ac45d0638c46baeb40170a35989f4
zackwolf <azhar.zakiy@gmail.com>
2019-01-20 14:07:32 +0800

    Rename Class to CamelCase rule

--- models/sys_role.py
+++ models/sys_role.py
@@ -1,10 +1,9 @@
 from connection import db, Required, PrimaryKey
 
 
-class Sys_role(db.Entity):
+class SysRole(db.Entity):
     _table_ = 'sys_role'
     sysrole_kode = PrimaryKey(str)
     sysrole_nama = Required(str)
 
 
-db.generate_mapping(create_tables=True)
