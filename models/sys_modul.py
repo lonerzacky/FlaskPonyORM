3d5ac35cbbddabd6eff4b8d24b033d38e1efbc44
zackwolf <azhar.zakiy@gmail.com>
2019-01-20 18:23:59 +0800

    Add Model Sys Modul

---
+++ models/sys_modul.py
@@ -0,0 +1,11 @@
+from connection import db, Required, PrimaryKey, Optional
+
+
+class SysModul(db.Entity):
+    _table_ = 'sys_modul'
+    sysmodul_kode = PrimaryKey(str)
+    sysmodul_nama = Required(str)
+    sysmodul_url = Required(str)
+    sysmodul_icon = Required(str)
+    sysmodul_parent = Optional(str,nullable=True)
+    sysmodul_no_urut = Required(int)
